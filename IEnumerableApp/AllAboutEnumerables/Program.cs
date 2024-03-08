6794e666d7fafacca8e31258c9f599b8583cac80
alex-toy <alexei.80@hotmail.fr>
2024-03-08 20:27:43 +0100

    ienumerables

--- IEnumerableApp/AllAboutEnumerables/Program.cs
+++ IEnumerableApp/AllAboutEnumerables/Program.cs
@@ -1,18 +1,22 @@
 ﻿
-int[] myArray = new int[] { 1, 2, 3, 4, 5 };
+//int[] myArray = new int[] { 1, 2, 3, 4, 5 };
 
-IEnumerable<int> myArrayAsEnumerable = myArray as IEnumerable<int>;
+//IEnumerable<int> myArrayAsEnumerable = myArray as IEnumerable<int>;
 
-foreach (var item in myArray) Console.WriteLine(item);
+//foreach (var item in myArray) Console.WriteLine(item);
 
-foreach (var item in myArrayAsEnumerable) Console.WriteLine(item);
+//foreach (var item in myArrayAsEnumerable) Console.WriteLine(item);
 
-//// we cannot use for loops with indexing on IEnumerables! indexing is not
-//// supported by IEnumerable, but it does work for arrays
+// we cannot use for loops with indexing on IEnumerables! indexing is not
+// supported by IEnumerable, but it does work for arrays
 //for (int i = 0; i < myArray.Length; i++) Console.WriteLine(myArray[i]);
 
 ////we can't even get the length of the enumerable!
-////for (int i = 0; i < myArrayAsEnumerable.Length; i++) Console.WriteLine(myArrayAsEnumerable[i]);
+//for (int i = 0; i < myArrayAsEnumerable.Length; i++) Console.WriteLine(myArrayAsEnumerable[i]);
+
+
+
+
 
 //List<int> myList = new List<int> { 6, 7, 8, 9, 10 };
 
@@ -22,29 +26,18 @@ foreach (var item in myArrayAsEnumerable) Console.WriteLine(item);
 
 //foreach (var item in myListAsEnumerable) Console.WriteLine(item);
 
-////// let's have a quick look at what this means for return types!
-//IEnumerable<string> FunctionThatReturnsAnArrayAsEnumerable()
-//{
-//    return new string[] { "A", "B", "C" };
-//}
+//// let's have a quick look at what this means for return types!
+IEnumerable<string> FunctionThatReturnsAnArrayAsEnumerable()
+{
+    return new string[] { "A", "B", "C" };
+}
 
-//IEnumerable<string> FunctionThatReturnsAListAsEnumerable()
-//{
-//    return new List<string> { "A", "B", "C" };
-//}
+IEnumerable<string> FunctionThatReturnsAListAsEnumerable()
+{
+    return new List<string> { "A", "B", "C" };
+}
 
-//// we can use foreach
-//Console.WriteLine("Using foreach on the array function...");
-//foreach (var item in FunctionThatReturnsAnArrayAsEnumerable())
-//{
-//    Console.WriteLine(item);
-//}
+// we can use foreach
+foreach (var item in FunctionThatReturnsAnArrayAsEnumerable()) Console.WriteLine(item);
 
-//Console.WriteLine("Using foreach on the list function...");
-//foreach (var item in FunctionThatReturnsAListAsEnumerable())
-//{
-//    Console.WriteLine(item);
-//}
-
-////// why would we want to do this? are there any implications?
-////// :) let's find out in the next part!
+foreach (var item in FunctionThatReturnsAListAsEnumerable()) Console.WriteLine(item);
--- IEnumerableApp/IEnumerableApp/Controllers/HomeController.cs
+++ IEnumerableApp/IEnumerableApp/Controllers/HomeController.cs
@@ -63,47 +63,10 @@ namespace IEnumerableApp.Controllers
 
         public IActionResult Enumerables()
         {
-            IEnumerable<ToDo> ienum_todos = _db.ToDos;
-            ICollection<ToDo> icoll_todos = _db.ToDos.ToList();
-            IList<ToDo> ilist_todos = _db.ToDos.ToList();
+            IEnumerable<Employee> employees = _db.Employees; // loading the employee table
+            IEnumerable<Employee> employees1 = employees.Where(e => e.DepartmentId == 2).ToList();
 
-            Console.WriteLine("** IEnumerable ******************************");
-            foreach (var todo in ienum_todos) Console.WriteLine($"{todo.Item}");
-
-            Console.WriteLine("** ICollection ******************************");
-            foreach (var todo in icoll_todos) Console.WriteLine($"{todo.Item}");
-
-            Console.WriteLine("** IList ******************************");
-            foreach (var todo in ilist_todos) Console.WriteLine($"{todo.Item}");
-
-            ToDo toDo = new ToDo() { Id = 5, Item = "Hello" };
-
-            //ienum_todos.Add(toDo); doesn't work
-            icoll_todos.Add(toDo);
-            ilist_todos.Add(toDo);
-
-            //ienum_todos.Remove(toDo); doesn't work
-            icoll_todos.Remove(toDo);
-            ilist_todos.Remove(toDo);
-
-            //ienum_todos.Insert(3, toDo); doesn't work
-            //icoll_todos.Insert(3, toDo); doesn't work
-            ilist_todos.Insert(3, toDo);
-
-            //ienum_todos.Insert(3); doesn't work
-            //icoll_todos.Insert(3); doesn't work
-            ilist_todos.RemoveAt(3);
-
-            //differed execution
-            var ienum_first1 = ienum_todos.FirstOrDefault();
-            var ienum_first2 = ienum_todos.FirstOrDefault(u => u.Item.Contains("c"));
-            var ienum_first3 = ienum_todos.Count();
-
-            var icoll_first1 = ienum_todos.FirstOrDefault();
-            var icoll_first2 = ienum_todos.FirstOrDefault(u => u.Item.Contains("c"));
-            var icoll_first3 = ienum_todos.Count();
-
-            return View();
+            return View("Index");
         }
 
         public IActionResult Privacy()

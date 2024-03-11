4eaa4dd517081dd4c9d625008ecd485f69437e09
alex-toy <alexei.80@hotmail.fr>
2024-03-11 20:07:25 +0100

    iterators

--- IEnumerableApp/IteratorBasics/Program.cs
+++ IEnumerableApp/IteratorBasics/Program.cs
@@ -1,46 +1,53 @@
 ﻿int[] dataset = new int[] { 1, 2, 3, 4, 5 };
 
 // function that returns the collection directly
-IEnumerable<int> SimpleEnumerable()
-{
-    return dataset;
-}
+//IEnumerable<int> SimpleEnumerable()
+//{
+//    return dataset;
+//}
 
-// function that is an "iterator"
-IEnumerable<int> SimpleIterator()
-{
-    foreach (var item in dataset) yield return item;
-}
+//// function that is an "iterator"
+//IEnumerable<int> SimpleIterator()
+//{
+//    foreach (var item in dataset) yield return item;
+//}
 
-foreach (var item in SimpleIterator()) Console.WriteLine(item);
-
-
-IEnumerable<int> ConsoleWritingIterator()
-{
-    Console.WriteLine("Printing to console at start of iterator!");
-    foreach (var item in dataset)
-    {
-        Console.WriteLine("Printing to console before yield return!");
-        yield return item;
-        Console.WriteLine("Printing to console after yield return!");
-    }
-
-    Console.WriteLine("Printing to console at end of iterator!");
-}
+//foreach (var item in SimpleIterator()) Console.WriteLine(item);
 
 
-Console.WriteLine("Foreach in console writing iterator...");
-foreach (var item in ConsoleWritingIterator())
-{
-    Console.WriteLine(item);
-}
+//IEnumerable<int> ConsoleWritingIterator()
+//{
+//    Console.WriteLine("start of iterator!");
+//    foreach (var item in dataset)
+//    {
+//        Console.WriteLine("before yield return!");
+//        yield return item;
+//        Console.WriteLine("after yield return!");
+//    }
 
-// some other neat properties? iterators are... lazy?!
-IEnumerable<int> FunctionThatSleepsFor5SecondsFirst()
-{
-    Thread.Sleep(5000);
-    return dataset;
-}
+//    Console.WriteLine("end of iterator!");
+//}
+
+//foreach (var item in ConsoleWritingIterator()) Console.WriteLine(item);
+
+
+
+// properties of iterators : lazy?!
+//IEnumerable<int> FunctionThatSleepsFor5SecondsFirst()
+//{
+//    Thread.Sleep(5000);
+//    return dataset;
+//}
+
+//Console.WriteLine($"Calling function that sleeps for 5 seconds first: {DateTime.Now}");
+//IEnumerable<int> resultA = FunctionThatSleepsFor5SecondsFirst();
+//Console.WriteLine($"Finished function that sleeps for 5 seconds first: {DateTime.Now}");
+
+//foreach (var item in resultA) Console.WriteLine(item);
+
+
+
+
 
 IEnumerable<int> IteratorThatSleepsFor5SecondsFirst()
 {
@@ -51,15 +58,8 @@ IEnumerable<int> IteratorThatSleepsFor5SecondsFirst()
     }
 }
 
-// this will wait the full 5 seconds because it is *not* an iterator, it is
-// simply a function that has an IEnumerable return type.
-Console.WriteLine($"Calling function that sleeps for 5 seconds first: {DateTime.Now}");
-var resultA = FunctionThatSleepsFor5SecondsFirst();
-Console.WriteLine($"Finished function that sleeps for 5 seconds first: {DateTime.Now}");
-
-// this assignment will happen "instantly" because it's an actual iterator (
-// uses yield return)... but... why?!
 Console.WriteLine($"Calling iterator that sleeps for 5 seconds first: {DateTime.Now}");
-var resultB = IteratorThatSleepsFor5SecondsFirst();
+IEnumerable<int> resultB = IteratorThatSleepsFor5SecondsFirst();
 Console.WriteLine($"Finished iterator that sleeps for 5 seconds first: {DateTime.Now}");
 
+foreach (var item in resultB) Console.WriteLine(item);

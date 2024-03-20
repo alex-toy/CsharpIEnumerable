838982c6d0148f4dbdf737fb7d3897f46b9f665d
alex-toy <alexei.80@hotmail.fr>
2024-03-20 20:04:22 +0100

    async

---
+++ IEnumerableApp/Asynchronisme/Program.cs
@@ -0,0 +1,19 @@
+﻿Task t1 = Task.Run(() =>
+{
+    Console.WriteLine("début t1");
+    Thread.Sleep(3000);
+    Console.WriteLine("milieu t1");
+    Thread.Sleep(3000);
+    Console.WriteLine("fin t1");
+});
+
+Task t2 = Task.Run(() =>
+{
+    Console.WriteLine("début t2");
+    Thread.Sleep(3000);
+    Console.WriteLine("milieu t2");
+    Thread.Sleep(3000);
+    Console.WriteLine("fin t2");
+});
+
+await Task.WhenAll(t1, t2);
